ee51529e3b177082b0645f8c7f2bb8d1d60a9923
sagevann <svann@whitepages.com>
2011-10-19 23:08:53 -0700

    loads of changes added functionality to sample sinatra app, added jsonp data services, basic stub is in for writing data

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :rubygems
 
+gem 'sinatra-jsonp', :require => 'sinatra/jsonp'
 gem 'rake'
 gem 'rspec'
 gem 'guard'
--- lib/sinatra.rb
+++ lib/sinatra.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
-
+require 'yaml'
+require 'sinatra/jsonp'
 # Sinatra has some good documentation
 # 
 # @see http://www.sinatrarb.com/
@@ -10,8 +11,25 @@ require 'sinatra'
 # you need to go over everything.
 # 
 
+
 get '/' do
-  "<h1>Hello World</h1>"
+  erb :index
+end
+
+get '/jsonp' do
+ data = YAML::load( File.open( './lib/public/yml/test.yml' ) )
+  JSONP data
+end
+
+post "/api" do
+  request.body.rewind  # in case someone already read it
+  #data = JSON.parse request.body.read
+  puts request.body.read
+end
+
+
+get '/hello/:name' do |n|
+  "<h1>Hello #{n}!</h1>"
 end
 
 get '/admin' do
